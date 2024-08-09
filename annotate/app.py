32614af704aa3e0b6a0c4183fcff8cc8610f0af3
Laya Dang <pd03@bu.edu>
2024-08-09 00:59:31 -0400

    got annotations to work, need to fix unique id issues and re-annotation prevention

---
+++ annotate/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import pandas as pd
+import csv
+
+df = pd.read_csv("./data/all_tweets.csv")
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'  # Needed to use sessions
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if 'tweet_index' not in session:
+        session['tweet_index'] = 0
+    if 'annotations' not in session:
+        session['annotations'] = []
+
+    tweet_index = session['tweet_index']
+
+    if request.method == 'POST':
+        annotation = request.form.get('annotation')
+
+        if annotation:
+            an_item = {'id': str(df['id'][tweet_index]), 'funny': annotation}
+            session['annotations'].append(an_item)
+            tweet_index += 1
+            session['tweet_index'] = tweet_index
+            
+
+    if session['annotations'] != []:
+        save_annotations(session['annotations'])
+
+    print(session['annotations'])
+
+    current_tweet = df['tweets'][tweet_index].replace('\n', '<br>')
+    current_id = df['id'][tweet_index]
+
+    return render_template('index.html', tweet=current_tweet, id=current_id)
+
+@app.route('/reset_session')
+def reset_session():
+    session.clear()  
+    return redirect(url_for('index'))  
+
+def save_annotations(annotations):
+    with open('./data/funny-annotations.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=['id', 'funny'])
+        writer.writeheader()
+        for annotation in annotations:
+            writer.writerow(annotation)
+
+if __name__ == '__main__':
+    app.run(debug=True,  host='127.0.0.1')
--- funny-tweets-scrape.py
+++ funny-tweets-scrape.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 import csv
 
-url = "https://www.funnytweeter.com/"
+url = "https://www.funnytweeter.com"
 all_tweets = []
 
 def get_tweets(url_page):
@@ -16,11 +16,26 @@ def get_tweets(url_page):
         # get only tweets without pictures 
         media_div = tweet.find('div', class_="media-fe flex-row")
         if not media_div:
-            tweet_text = tweet.get_text()
+            tweet_text = tweet.get_text().strip()
             all_tweets.append(tweet_text)
-            print(tweet_text)
+            # print(tweet_text)
 
 def save_tweets():
     # save array to csv
     pd.DataFrame({"tweets": all_tweets}).to_csv('data/all_tweets.csv', index=False)
 
+# how many pages to scrape
+pages = 100
+page_prefix = "/page/"
+
+def main():
+    # get first page of tweets
+    get_tweets(url)
+    for i in range(2, pages):
+        new_url = url + page_prefix + str(i)
+        get_tweets(new_url)
+        save_tweets()
+        print(i)
+main()
+
+df = pd.read_csv("data/all_tweets.csv")
